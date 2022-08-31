a1cfc02abf7a56e8c25cca26da6ce2e01853244e
AnnaKarollinne <111257168+AnnaKarollinne@users.noreply.github.com>
2022-08-31 14:11:02 -0300

    Add files via upload

--- Objet_Func/Objetos_funcoes.R
+++ Objet_Func/Objetos_funcoes.R
@@ -1,9 +1,10 @@
-#O vetor abaixo refere-se as nacionalidades dos migrantes oriundos de três países:
+#O vetor abaixo refere-se as nacionalidades dos migrantes oriundos de tres pa�ses:
 migrante<- c("venezuela", "haiti","ucrania")
-
+migrante
 #Regressão Linear
+impeachment_dilma
 regWord<- lm(impeachment_dilma$numeroPalavras~impeachment_dilma$numeroCaracteres)
-
+regWord
 #Complexidade dos Objetos
 str(migrante)
 str(regWord)
