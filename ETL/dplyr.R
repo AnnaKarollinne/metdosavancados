39085959bcf7f1cfbf8ef70a020bdc7a400edaea
AnnaKarollinne <111257168+AnnaKarollinne@users.noreply.github.com>
2022-09-06 14:49:06 -0300

    Add files via upload

---
+++ ETL/dplyr.R
@@ -0,0 +1,39 @@
+#Mais fatores
+
+#install.packages("ade4")
+#install.packages("arules")
+#install.packages("forcats")
+
+library(ade4)
+library(dplyr)
+library(arules)
+library(forcats)
+
+load(file = "impeachment_dilma.rda")
+
+# Sumário
+count(impeachment_dilma,partido)
+impeachment_dilma$uf
+
+#Sumário com agrupamento
+#dividir em grupos por partido
+grupo <- group_by(impeachment_dilma,partido)
+
+#mostra a média por partido
+summarise(grupo,avg =  mean(numeroPalavras))
+
+#transformação de casos
+#filtra para pegar todos menos pt
+filtro = filter(impeachment_dilma,partido != "pt")
+#calcula a média
+summarise(filtro,avg = mean(numeroPalavras))
+
+#agrupa
+grupo <- group_by(filtro,uf,partido)
+summarise(grupo,avg = mean(numeroPalavras))
+
+#ordenar
+arrange(impeachment_dilma,numeroPalavras)
+
+#Novas colunas
+mutate(impeachment_dilma,numeroCaracteres/numeroPalavras)
