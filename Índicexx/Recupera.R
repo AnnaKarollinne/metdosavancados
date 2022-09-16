d3486e0af710420951d3f53f659b6e6332fd3f0b
AnnaKarollinne <111257168+AnnaKarollinne@users.noreply.github.com>
2022-09-16 15:54:23 -0300

    Update Recupera.R

--- Índicexx/Recupera.R
+++ Índicexx/Recupera.R
@@ -1,4 +1,5 @@
 #Carregar dados
+impeachment_dilma<- fread('https://github.com/AnnaKarollinne/metdosavancados/blob/main/ETL/impeachment_dilma.rda')
 load("impeachment_dilma.rda")
 #Index
 impeachment_dilma$nomeOrador[10]
