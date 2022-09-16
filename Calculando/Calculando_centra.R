61aae16835779f719811db374d491dca112f0be0
AnnaKarollinne <111257168+AnnaKarollinne@users.noreply.github.com>
2022-09-16 16:31:13 -0300

    Update Calculando_centra.R

--- Calculando/Calculando_centra.R
+++ Calculando/Calculando_centra.R
@@ -1,4 +1,5 @@
-#Carregar dado
+#Carregar dados
+impeachment_dilma<-fread("https://github.com/AnnaKarollinne/metdosavancados/blob/main/Calculando/impeachment_dilma.rda")
 load("impeachment_dilma.rda")
 #Calculando
 mediaPT<- mean(impeachment_dilma$numeroPT)
