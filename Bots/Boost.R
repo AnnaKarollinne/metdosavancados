77a9a25e6cca8be0f757ae48341fdeaf99c8db80
AnnaKarollinne <111257168+AnnaKarollinne@users.noreply.github.com>
2022-09-23 16:27:32 -0300

    Update Boost.R

--- Bots/Boost.R
+++ Bots/Boost.R
@@ -1,7 +1,13 @@
-normal<-rnorm(20, mean = 50,sd=2)
+normal<-rnorm(100)
 #Bootstrapping
 boots<-sample(normal,15,replace = TRUE)
 boots
 hist(boots)
 bootsDistNormal<- replicate(15, boots)
-boostdDistNormal
+bootsDistNormal
+
+bootsDistNormal15<-replicate(15,boots)
+bootsDistNormal95<-replicate(95,boots)
+mean(bootsDistNormal15)
+mean(bootsDistNormal95)
+mean(normal)
