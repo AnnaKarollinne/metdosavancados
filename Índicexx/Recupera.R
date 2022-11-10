ff5956d6c7d9d1d0158b3e781a4f1e1ac9545528
AnnaKarollinne <111257168+AnnaKarollinne@users.noreply.github.com>
2022-11-10 15:40:33 -0300

    Update Recupera.R

--- Índicexx/Recupera.R
+++ Índicexx/Recupera.R
@@ -2,6 +2,5 @@
 load(url("https://github.com/AnnaKarollinne/metdosavancados/blob/main/%C3%8Dndicexx/impeachment_dilma.rda?raw=true"))
 #Index
 impeachment_dilma$nomeOrador[10]
-#ou em caso de busca no console
-index<- scan()
-impeachment_dilma$nomeOrador[index]
+#usando a função which para localizar a posição de PMDB
+which(impeachment_dilma$partido == "PMDB")
